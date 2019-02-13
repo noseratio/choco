924a3a8e918d6d4245ea1d26eeb705b3363ba1dc
Andrew Nosenko <andrew.nosenko@nuance.com>
2019-02-13 17:00:56 +1030

    console title

--- wsudo/bin/wsudo.ps1
+++ wsudo/bin/wsudo.ps1
@@ -1,49 +1,58 @@
-#Requires -Version 5.0
+#Requires -Version 3.0
 
 param(
   [Parameter(Mandatory = $true)] [ValidateSet('prepare','run')] [string] $action,
-  [string] $processInfo,
-  [switch] $stay)
+  [string] $title,
+  [string] $command,
+  [switch] $stayOpen)
 
 Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
 
-class Process {
+class CommandInfo {
   [string]$curDir
   [string]$comSpec
   [string]$commandLine
-  [bool]$stay
+  [string]$title
+  [bool]$stayOpen
 }
 
 switch($action) {
   'prepare' {
     # pack curDir, commandLine etc using JSON/base64 and re-launch itself as admin
-    $process = [Process]::new()
-    $process.comSpec = $env:ComSpec
-    $process.commandLine = $env:wsudo_commandLine
-    $process.curDir = $env:wsudo_curDir
-    $process.stay = $stay
-    $json = ConvertTo-Json -Compress $process
-    $encoded = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($json))
+    $commandInfo = [CommandInfo]::new()
+    $commandInfo.comSpec = $env:ComSpec
+    $commandInfo.commandLine = $env:wsudo_commandLine
+    $commandInfo.curDir = $env:wsudo_curDir
+    $commandInfo.title = $title
+    $commandInfo.stayOpen = $stayOpen
+    $commandInfoJson = ConvertTo-Json -Compress $commandInfo
+    $commandInfoEncoded = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($commandInfoJson))
     # get the powershell EXE name
     $powershell = [System.Diagnostics.Process]::GetCurrentProcess().MainModule.FileName
     $pi = Start-Process -PassThru -Verb runAs `
-      -FilePath $powershell -ArgumentList "-f $PSCommandPath -action run -processInfo $encoded"
+      -FilePath $powershell -ArgumentList "-f $PSCommandPath -action run -commandInfo $commandInfoEncoded"
     $pi.WaitForExit()
     exit $pi.ExitCode
     break
   }
   'run' {
     # unpack curDir, commandLine etc and run the command under CMD
-    if (!$processInfo) { 
-      throw 'Must provide process info.'
+    if (!$command) { 
+      throw 'Must provide commandInfo.'
     }
-    $decoded = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($processInfo));
-    $process = [Process](ConvertFrom-Json $decoded)
-    Set-Location $process.curDir
-    $opt = if ($process.stay) { '/K' } else { '/C' }
+    
+    $commandInfoJson = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($command));
+    $commandInfo = [CommandInfo](ConvertFrom-Json $commandInfoJson)
+
+    if ($commandInfo.title) { 
+      $host.ui.RawUI.WindowTitle = $commandInfo.title
+    }
+
+    Set-Location $commandInfo.curDir
+    $opt = if ($commandInfo.stayOpen) { '/K' } else { '/C' }
     $pi = Start-Process -PassThru -NoNewWindow `
-      -FilePath $process.comSpec -ArgumentList "$opt $($process.commandLine)"
+      -FilePath $commandInfo.comSpec -ArgumentList "$opt $($commandInfo.commandLine)"
     $pi.WaitForExit()
     exit $pi.ExitCode
     break
--- wsudo/bin/wsudox.cmd
+++ wsudo/bin/wsudox.cmd
@@ -2,5 +2,5 @@
 setlocal
 set wsudo_commandLine=%*
 set wsudo_curDir=%CD%
-powershell.exe -f "%~dp0.\wsudo.ps1" -action prepare
+powershell.exe -f "%~dp0.\wsudo.ps1" -title %~n0 -action prepare
 exit /b %ERRORLEVEL%
