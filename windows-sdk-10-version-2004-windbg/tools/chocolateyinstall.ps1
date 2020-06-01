768e5b5938ed14ca08d6df750fd1028bca6658d3
Andrew Nosenko <andrew.nosenko@nuance.com>
2020-06-01 21:15:01 +1000

    WinSDK for Windows 10 v2004

---
+++ windows-sdk-10-version-2004-windbg/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$packageName = 'windows-sdk-10-version-2004-windbg'
+$installerType = 'EXE'
+$url = 'https://download.microsoft.com/download/1/c/3/1c3d5161-d9e9-4e4b-9b43-b70fe8be268c/windowssdk/winsdksetup.exe'
+$checksum = 'd53f651370f87484b78622e30dfb1a41920b501e4041035771c0d785561f47d5'
+$checksumType = 'sha256'
+$silentArgs = "/Features OptionId.WindowsDesktopDebuggers /Quiet /NoRestart /Log ""$env:temp\${packageName}_$([Guid]::NewGuid().ToString('D')).log"""
+$validExitCodes = @(0,3010)
+
+$posParams = @(
+  $packageName,
+  $installerType,
+  $silentArgs,
+  $url)
+
+$parms = @{
+  'checksum' = $checksum;
+  'checksumType' = $checksumType;
+  'validExitCodes' = $validExitCodes}
+
+Install-ChocolateyPackage @posParams @parms
