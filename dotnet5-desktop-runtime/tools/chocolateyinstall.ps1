4a6e3c4b252dfa7fbc6f5c38f706f3e5bf49e765
Tobias Tiederle <ttiederle@fimltd.org>
2021-05-14 11:02:06 +0200

    update dotnet5-desktop-runtime to 5.0.6

--- dotnet5-desktop-runtime/tools/chocolateyinstall.ps1
+++ dotnet5-desktop-runtime/tools/chocolateyinstall.ps1
@@ -2,17 +2,17 @@
   packageName    = 'dotnet5-desktop-runtime'
   fileType       = 'exe'
 
-  url            = 'https://download.visualstudio.microsoft.com/download/pr/0c5c56a4-8b34-4361-8af9-482c788b2bcf/d734b200547c8c367eb45ebbd69c4698/windowsdesktop-runtime-5.0.4-win-x86.exe'
-  checksum       = 'f9dc79182c557751ad8590aa70fdf01e10b691d95910520e9323074ba6e3bae0df6dc60c084e01536e7b46b6617e4173300ad0d541d4ba6bc18b42f54bf0a051'
+  url            = 'https://download.visualstudio.microsoft.com/download/pr/315854e8-6857-4d0d-b7e0-57761e3f7d12/b31193ac2c9f1674b66cf7a65c2521de/windowsdesktop-runtime-5.0.6-win-x86.exe'
+  checksum       = '83ceb900805a33763df0a16e81f3f3d2a4b576802b335975177d77953bf9f0db60d36f257b2e018fe44440df2b8f8a8c44e767939bd9e4ff21230eb32f34db66'
   checksumType   = 'sha512'
 
-  url64bit       = 'https://download.visualstudio.microsoft.com/download/pr/7a5d15ae-0487-428d-8262-2824279ccc00/6a10ce9e632bce818ce6698d9e9faf39/windowsdesktop-runtime-5.0.4-win-x64.exe'
-  checksum64     = '69fcbe5458869246cbd0657764ec3fdbfd59f06a49808cb283a7c90271409b0266539d31cec9047c512f167cae8e2992b617b68bc9f93e4815a37a8f50524d3f'
+  url64bit       = 'https://download.visualstudio.microsoft.com/download/pr/6279dc90-f437-4481-82a5-73dd9f97da06/6519ef44735fd31115b9b1a81d6ff1e8/windowsdesktop-runtime-5.0.6-win-x64.exe'
+  checksum64     = '34b0d43e03b5aaeb987bbc9d6d6ee95b79d97587fecf39d8b8b653618cec4d8741c8d31c270662a0c8b59a18344be2a44b7c38e525505270b264d7dcc856761c'
   checksumType64 = 'sha512'
 
   silentArgs     = "/install /quiet /norestart /Log ""$env:temp\${packageName}_$([Guid]::NewGuid().ToString('D')).log"""
   validExitCodes = @(0,3010)
-  softwareName   = '.NET 5 Desktop Runtime v5.0.4'
+  softwareName   = '.NET 5 Desktop Runtime v5.0.6'
 }
 
 Install-ChocolateyPackage @packageArgs
