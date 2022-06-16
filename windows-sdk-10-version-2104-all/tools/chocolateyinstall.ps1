04d61c386da17a8b058f7d3137197841ea8f5ffd
Andrew Nosenko <noseratio@gmail.com>
2022-06-17 07:34:30 +1000

    SDK for Windows 10 Version 2104 (10.0.20348.0)

---
+++ windows-sdk-10-version-2104-all/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'windows-sdk-10-version-2104-all'
+$installerType = 'EXE'
+$url = 'https://download.microsoft.com/download/9/7/9/97982c1d-d687-41be-9dd3-6d01e52ceb68/windowssdk/winsdksetup.exe'
+$checksum = 'bc73435923977350ac92420a06c0d5a7db3a966ce66393c3218accc6b1b4076d'
+$checksumType = 'sha256'
+$silentArgs = "/Features + /Quiet /NoRestart /Log ""$env:temp\${packageName}_$([Guid]::NewGuid().ToString('D')).log"""
+$validExitCodes = @(0,3010)
+Install-ChocolateyPackage $packageName $installerType $silentArgs $url -checksum $checksum -checksumType $checksumType -validExitCodes $validExitCodes
